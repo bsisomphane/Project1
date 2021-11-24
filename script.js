d1acadeed367100b10709df4d6b0831b240c863e
Charlie Stedman <9369467+cbs9646@users.noreply.github.com>
2021-11-24 10:42:48 -0600

    updated js with fetch code

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let currencySymbol = "EUR"
-let amountBlank = "1200"
+
 
 
 let exchangeRates = `https://api.exchangerate.host/latest?base=USD&symbols=${countryCode}&amount=${amountBlank}`
@@ -13,3 +12,35 @@ fetch(exchangeRates, {
        console.log("currency data");
        console.log(currencyData.rates);
    });
+
+let countryText = document.querySelector('#city-text');
+let currencySubmitBtn = document.querySelector('#currency-submit');
+
+currencySubmitBtn.addEventListener('click', currencySubmit);
+// Hooking into user input
+function currencySubmit () {
+    // event.preventDefault();
+    console.log("This is the searchSubmit Test");
+    let userCity = document.querySelector("#city-text").value;
+    // let weatherInCity = "Sunny"
+    if (!userCity) {
+      console.error('Please enter an amount in $USD');
+      return;
+    }
+   citySearch(userCity);
+  }
+console.log(cityText);
+// Function call for weather api
+function citySearch (userCity) {
+    let cityCoords = `https://api.openweathermap.org/data/2.5/weather?q=${userCity}&appid=f85be298446b14abaf2660208bb00bf7`;
+    fetch(cityCoords)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (fiveDayData) {
+          console.log("This is the openWeather fetch.")
+          console.log(fiveDayData);
+          oneCall(fiveDayData);
+          fiveDayWeather(fiveDayData);
+      });
+    };
