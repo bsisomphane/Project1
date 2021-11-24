ecc3a128fe791967f7ee28b39d9c7a67dae051f4
T0mWhite <tbw2.mail@gmail.com>
2021-11-24 12:52:50 -0600

    Populating card with city name and landmarks - test

--- script.js
+++ script.js
@@ -1,32 +1,16 @@
-// let cityCoords = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=f85be298446b14abaf2660208bb00bf7`
+// Hooking into submit button to begin fetch
+let citySubmitBtn = document.querySelector('#citySubmitBtn');
 
-// fetch(cityCoords, {
-// })
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (fiveDayData) {
-//       console.log("This is the openWeather fetch for lat and lon")
-//       console.log(fiveDayData.coord);
-//       console.log(fiveDayData.coord.lon);
-//       console.log(fiveDayData.coord.lat);
-  
-//       return fetch(`https://www.airnowapi.org/aq/forecast/latLong/?format=json&latitude=${fiveDayData.coord.lat}&longitude=${fiveDayData.coord.lon}&date=2021-11-23&distance=25&API_KEY=7880F6AC-7477-433F-9C85-ECDA8C61EEA0`)
-//   })
-//   .then(function (airNowData) {
-//       return airNowData.json();
-//   })
-//   .then(function (airNowJson) {
-//       console.log(airNowJson);
-//   });
-
-let citySubmitBtn = document.querySelector('#city-name');
-citySubmitBtn.addEventListener('click', citySearch);
+// citySubmitBtn.addEventListener('click', citySearch(event) {
+//   event.preventDefault()
+// });
+citySubmitBtn.addEventListener("click", function(event){
+  event.preventDefault();
+  citySearch();
+});
 
 
- 
-
-// Hooking into user input for the city name
+// Hooking into user input for the city name to populate fetch url
 function citySearch () {
   console.log("This is the city search Test");
   let userCity = document.querySelector("#city-text").value;
@@ -45,13 +29,41 @@ function apiSearch (userCity) {
 		"x-rapidapi-host": "hotels4.p.rapidapi.com",
 		"x-rapidapi-key": "21f4c0498cmsh813bdb469f313d1p130468jsn583f4c713308"
 	}
-  })
+})
   .then(function (response) {
-    console.log("This is the response from the apiSearch function");
+    console.log("apiSearch function ");
     return response.json();
   })
   .then(function (hotelData) {
-    console.log("Successful fetch from hotel API");
+    console.log("Successful fetch from hotel API. Fetch data is stored in hotelData");
     console.log(hotelData);
+    populateLMS(hotelData);
   })
-};
+}
+
+function populateLMS(hotelData) {
+  for (let i = 0; i < hotelData.suggestions[2].entities.length; i++) {
+    const landmarks = hotelData.suggestions[2].entities[i].name;
+    console.log(landmarks);
+  }
+  let cityName = document.querySelector(".city-name");
+  let cityLM = document.querySelector(".city-lm");
+  cityName.innerHTML = hotelData.term;
+  console.log("City and Country populated.");
+  cityLM.innerHTML = hotelData.landmarks;
+  console.log(hotelData.landmarks);
+}
+
+
+
+// console.log(hotelData.suggestions[2].entities[1].name);
+
+/* <div class="row">
+    <div class="col-sm-6">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title city-name">Weather</h5>
+          <p class="card-text city-lm">todays weather is blah blah blah</p>
+        </div>
+      </div>
+    </div> */
