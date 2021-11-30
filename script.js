0a918ba0873177b1357c55b0bfda25a8f0e2d8f6
T0mWhite <tbw2.mail@gmail.com>
2021-11-29 20:56:34 -0600

    Able to access the country code as anattribute.

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 // Declaring constant variables to pass select form information
+// let currencyCode = document.querySelector(".form-select");
+// console.log(currencyCode);
+// let newCurrencyCode = currencyCode.toString();
 
-const city1 = document.querySelector("#city1");
-const city2 = document.querySelector("#city2");
-const city3 = document.querySelector("#city3");
-const city4 = document.querySelector("#city4");
-const city5 = document.querySelector("#city5");
-const city6 = document.querySelector("#city6");
-const city7 = document.querySelector("#city7");
+
+
+// console.log(currencyCode.getAttribute(class));
+// console.log(newCurrencyCode);
+
+// const city1 = document.querySelector("#city1");
+// const city2 = document.querySelector("#city2");
+// const city3 = document.querySelector("#city3");
+// const city4 = document.querySelector("#city4");
+// const city5 = document.querySelector("#city5");
+// const city6 = document.querySelector("#city6");
+// const city7 = document.querySelector("#city7");
 
 // Hooking into submit button to begin fetch
 const citySubmitBtn = document.querySelector('#citySubmitBtn');
@@ -27,22 +35,22 @@ function citySearch(userCity) {
   } else {
     console.log(`Function citySearch executed. Value searched is ${userCity}.`);
   }
- rapidApiSearch(userCity);
- console.log("rapidApiSearch called with userCity as a parameter.")
+  rapidApiSearch(userCity);
+  console.log("rapidApiSearch called with userCity as a parameter.")
 }
 
 
 function rapidApiSearch (userCity) {
   let rapidApiData = `https://hotels4.p.rapidapi.com/locations/v2/search?query=${userCity}&locale=en_US&currency=USD`
-// Fetching from rapidApiData url
+  // Fetching from rapidApiData url
   fetch(rapidApiData, 
     {
     "method": "GET",
     "headers": {
-    "x-rapidapi-host": "hotels4.p.rapidapi.com",
-    "x-rapidapi-key": "21f4c0498cmsh813bdb469f313d1p130468jsn583f4c713308"
-  }
-})
+      "x-rapidapi-host": "hotels4.p.rapidapi.com",
+      "x-rapidapi-key": "21f4c0498cmsh813bdb469f313d1p130468jsn583f4c713308"
+    }
+  })
   .then(function (response) {
     console.log("rapidApi fetch request sent.");
     return response.json();
@@ -59,49 +67,68 @@ function populateLMCard(landmarkData) {
   let cityLM = document.querySelector(".city-lm");
   cityName.textContent = landmarkData.term;
     // if (landmarkData.term = text.includes("Barcelona")) {
-    //   let landmarkImg = document.createElement('img');
+      //   let landmarkImg = document.createElement('img');
     //   // landmarkImg.innerHTML
     //   cityName.appendChild(landmarkImg);
     // }
-  console.log("City name populated in card.");
-  cityLM.textContent = "Check out the top landmarks:";
+    console.log("City name populated in card.");
+    cityLM.textContent = "Check out the top landmarks:";
     for (let landmarkIndex = 0; landmarkIndex < 3; landmarkIndex++) {
       let landmarkItem = document.createElement('p');
       cityLM.appendChild(landmarkItem);
       landmarkItem.textContent = landmarkData.suggestions[2].entities[landmarkIndex].name;
     }
-};
+  };
+  
+  
+  
 
 
 
 
 
 
-// currency submit buttons; returns value from currency box
-let currencySubmitBtn = document.getElementById('currencyExch');
-currencySubmitBtn.addEventListener("submit", function(event){
-  console.log("CurrentcySubmitBtn event listener test.")
-  event.preventDefault();
-  currencySubmit(event);
-});
 
-// Hooking into user input
 
-function currencySubmit () {
+  const currencySubmitBtn = document.querySelector("#currencyExch");
+  
+  // currencySubmitBtn calls currencySubmit function on click
+  currencySubmitBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    let currency = document.querySelector(".form-select").value;
+    console.log(currency);
+    currencySubmit(currency);
+  });
+
+  // Hooking into user input
+  
+  function currencySubmit(currency) {
     console.log("This is the currencySubmit Test");
     let amountBlank = document.getElementById("currency-textbox").value;
     console.log(amountBlank)
     if (!amountBlank) {
       console.error('Please enter an amount in Dollars');
       return;
-
+      
     }
-   currencyRate(amountBlank);
+    currencyRate(amountBlank);
   }
+  
+  // Defining code as the country's currency code.
+  
+  let citySelection = document.querySelector("#citySelect");
+    
+  citySelection.onchange = function(event){
+    let code = event.target.options[event.target.selectedIndex].dataset.code;
+    console.log("Country code: " + code);
+    return code;
+  };
 
+  console.log(code);
   // Function call for currency api
 function currencyRate (amountBlank) {
-let currencyApi = `https://api.exchangerate.host/latest?base=USD&symbols=${currencyCode}&amount=${amountBlank}`;    
+  
+let currencyApi = `https://api.exchangerate.host/latest?base=USD&symbols=${code}&amount=${amountBlank}`;    
     fetch(currencyApi)
       .then(function (response) {
         return response.json();
@@ -112,7 +139,6 @@ let currencyApi = `https://api.exchangerate.host/latest?base=USD&symbols=${curre
 
 console.log(currencyApi);
 };
-
 
 
 
